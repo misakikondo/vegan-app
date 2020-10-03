da38d7c3ea9606420e662613411f45fea2682695
misakikondo <misakikondo718@gmail.com>
2020-10-03 14:27:03 +0900

    Basic認証修正
    BASIC_AUTH_USER、BASIC_AUTH_PASSWORDの表記誤りを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV["SECRET_USER"] && password == ENV["SECRET_KEY"]
     end
   end
 end
