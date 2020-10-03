306ca44b46599c3b8a7babd9faa6e845ad96cac1
misakikondo <misakikondo718@gmail.com>
2020-10-03 15:32:18 +0900

    Userモデルバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :recipes
+  has_many :donates
+  has_many :comments
+
+  validates :email, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },
+                       length: { minimum: 6 }
+
+  validates :nickname, presence: true
+
+
+  validates :birth, presence: true
 end
+
