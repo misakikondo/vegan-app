9d1e8f300d9f0df83938fde66a0dd8037df70c79
misakikondo <misakikondo718@gmail.com>
2020-10-05 10:53:59 +0900

    Recipesテーブル設計

--- db/migrate/20201004113459_create_recipes.rb
+++ db/migrate/20201004113459_create_recipes.rb
@@ -1,7 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-
+      t.string       :recipes_name,              null: false
+      t.text         :explains,                  null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_03_050126) do
+ActiveRecord::Schema.define(version: 2020_10_04_113459) do
+
+  create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "recipes_name", null: false
+    t.text "explains", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
