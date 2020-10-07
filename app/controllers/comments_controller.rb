16a640d725d03d399971f8278e435ce11e45d961
misakikondo <misakikondo718@gmail.com>
2020-10-07 16:51:25 +0900

    showアクションの追加
    commentsコントローラーにshowアクションを追加、コメントが保存されるように設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to recipe_path(@comment.recipe.id)
+    else
+      render template: "recipes/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:contents).merge(user_id: current_user.id, recipe_id: params[:recipe_id])
+  end
 end
