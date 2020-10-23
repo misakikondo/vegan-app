b3eb6a57e8a13b9caca877a5fd54ab4625022bfe
misakikondo <misakikondo718@gmail.com>
2020-10-23 12:13:57 +0900

    fixed association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :recipe, dependent: :destroy
-  belongs_to :user,   dependent: :destroy
+  belongs_to :recipe
+  belongs_to :user
 
   validates :contents, presence: true, length: { maximum: 100 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :level
   belongs_to :cookingtime
   belongs_to :user
-  has_many   :comments
+  has_many   :comments, dependent: :destroy
   has_one_attached :image
   has_many   :likes, dependent: :destroy
   has_many   :liked_users, through: :likes, source: :user
