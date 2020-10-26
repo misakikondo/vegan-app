13ef3bd0b82fb513720420afdc2e8c4e506692c4
misakikondo <misakikondo718@gmail.com>
2020-10-26 11:36:56 +0900

    updated recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!,  only: :new
+  before_action :authenticate_user!,  only: [:new, :show]
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
   before_action :search_recipe, only: [:index, :search]
 
